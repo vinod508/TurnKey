17bbf3250378a5177357a9448e9706bf95ce4395
Vinod <vinod@vpstechnology.in>
2025-05-30 18:35:19 +0530

    Fix: Include both default and Style2 themes

--- obj/Debug/net8.0/TurnKey.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurnKey.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnKey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd8945f323a8bb4c96777ac9b5b9ac4b13b458b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnKey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnKey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
